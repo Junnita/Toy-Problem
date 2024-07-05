7483646634484c63ff1d22a7c1e6666a2e17eb06
junnewanja-cyber <junewanja8@gmail.com>
2024-07-05 23:35:19 +0300

    Update challenge2.js

--- challenge2.js
+++ challenge2.js
@@ -3,7 +3,7 @@ function speedDetector (speed){
         console.log ("ok");
     } else {
 
-        let demeritsPoints = Math.floor((speed - 70 ) /5);
+        let demeritsPoints = ((speed - 70 ) /5);
         if (demeritsPoints < 12) {
 
         }else {
